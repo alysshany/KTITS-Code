369ca2b1813e8906476d4b905b5d2626b8c47f24
Zilya <galievaaziz@gmail.com>
2023-06-09 13:05:35 +0300

    added blazor app

---
+++ LotsBlazorApp/LotsBlazorApp/Data/DBConnection.cs
@@ -0,0 +1,55 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver.GridFS;
+using MongoDB.Driver;
+
+namespace LotsBlazorApp.Data
+{
+    public class DBConnection
+    {
+        public void AddToDataBase(LotInfo lot)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("Lots");
+            var collection = database.GetCollection<LotInfo>("CollectionOfLots");
+            collection.InsertOne(lot);
+        }
+
+        public LotInfo FindByTitle(string title)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("Lots");
+            var collection = database.GetCollection<LotInfo>("CollectionOfLots");
+            var lot = collection.Find(x => x.NumberOfLot == title).FirstOrDefault();
+            return lot;
+        }
+
+        public void SavingImageToDataBase(byte[] imageBytes, string lotTitle)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("LotsImages");
+            var gridFSBucket = new GridFSBucket(database);
+            // Сохранение изображения в базу данных
+            var id = ObjectId.GenerateNewId();
+            var options = new GridFSUploadOptions
+            {
+                Metadata = new BsonDocument
+                        {
+                            { "filename", lotTitle }
+                        }
+            };
+            using (var stream = new MemoryStream(imageBytes))
+            {
+                gridFSBucket.UploadFromStream(id, lotTitle, stream, options);
+            }
+        }
+
+        public List<LotInfo> ImportAllLots()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("Lots");
+            var collection = database.GetCollection<LotInfo>("CollectionOfLots");
+            var list = collection.Find(new BsonDocument()).ToList();
+            return list;
+        }
+    }
+}
---
+++ LotsBlazorApp/LotsBlazorApp/Data/LotInfo.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace LotsBlazorApp.Data
+{
+    public class LotInfo
+    {
+        [BsonIgnoreIfDefault]
+        public ObjectId Id { get; set; }
+        public string NumberOfLot { get; set; }
+        public string Title { get; set; }
+        public string StartingPrice { get; set; }
+        public string AuctionStep { get; set; }
+        public string LotType { get; set; }
+        public string LotSub { get; set; }
+        public string LotDescription { get; set; }
+        public string LocationSubject { get; set; }
+        public string Location { get; set; }
+        public string ObjectCategory { get; set; }
+        public string OwnershipForm { get; set; }
+        public string LandPurpose { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
